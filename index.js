282293ca80a7d7eb1bf701ef1cf91daf785a6a8d
Max Cantlupe <maxwellcantlupe@gmail.com>
2026-03-06 18:11:17 -0500

    Complete JavaScript scope lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+// Global scope: burgers array with const
+const burgers = ['Hamburger', 'Cheeseburger'];
+
+// Global scope: featuredDrink with let
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function that adds a new burger (function-scoped variable)
+function addBurger() {
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+}
+
+// If statement with block-scoped variable
+if(true) {
+  const anotherNewBurger = 'Maple Bacon Burger';
+  burgers.push(anotherNewBurger);
+}
+
+// Function that changes the featuredDrink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
